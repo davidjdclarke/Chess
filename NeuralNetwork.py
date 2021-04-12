65cc40a3605e7478531370907f7df061c1329d24
David Clarke <david.j.d.clarke@gmail.com>
2021-04-12 19:08:10 -0400

    Improvments to testEngine in testEngine2
    testEngine is now redundant

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -8,75 +8,28 @@ PROMOTIONS = {'Q': 5, 'R': 4, 'B': 3, 'N': 2}
 
 def testEngine():
     pgn_file = "./games/ficsgamesdb_202101_standard2000_nomovetimes_196479.pgn"
-    all_games = Preprocessor.getGames(pgn_file=pgn_file)
-    j = 0
-    scores = []
-    for game in all_games:
-        found_moves = 0
-        j += 1
-        print("")
-        print("Game No. " + str(j))
+    games = Preprocessor.getGames(pgn_file=pgn_file)
+    total_fail = 0
+    for num, game in enumerate(games):
         gs = ChessEngine.GameState()
-        game_moves = game.Moves
-        total_moves = len(game_moves)
-        for move in game_moves:
-            if gs.whiteToMove:
-                print('White Move: ' + str(move), end=" ")
-            else:
-                print('Black Move: ' + str(move), end=" ")
-            if '+' in move:
-                move = move.strip('+') 
-                print(" -(+)", end=" ")
-            elif '#' in move:
-                move = move.strip('#')
-                print(" -(#)", end=" ")
-            possible_moves = gs.getValidMoves()
-            selected_move = None
-            for i in range(len(possible_moves)):
-                if move == possible_moves[i].moveString:
-                    selected_move = possible_moves[i]
-                    print("[x]")
-                    found_moves += 1
+        moves = game.Moves
+        score = 0
+        for i, move in enumerate(moves):
+            next_move = gs.getMoveFromString(move)
+            try:
+                gs.makeMove(next_move)
+                score += 1
+            except:
+                if move == '':
                     break
-            if selected_move == None and move != '':
-                if move[0] in ['N', 'B', 'R']:
-                    new_move = move[0] + move[2:]
-                    if move[1].isnumeric():
-                        row = int(move[1]) - 1
-                        for i in range(len(possible_moves)):
-                            if new_move == possible_moves[i].moveString and possible_moves[i].startRow == row:
-                                selected_move = possible_moves[i]
-                                print("[x]")
-                                found_moves += 1
-                                break
-                    elif move[1] in ['a', 'b', 'c', 'd', 'e', 'g', 'f', 'h']:
-                        col = COLUMNS[move[1]]
-                        for i in range(len(possible_moves)):
-                            if new_move == possible_moves[i].moveString and possible_moves[i].startCol == col:
-                                selected_move = possible_moves[i]
-                                print("[x]")
-                                found_moves += 1
-                                break
-            if '=' in move:
-                choice = move[-1]
-                move = move[0:-2]
-                for i in range(len(possible_moves)):
-                    if move == possible_moves[i].moveString and possible_moves[i].isPawnPromotion:
-                        f = 1 if gs.whiteToMove else (-1)
-                        possible_moves[i].promotionChoice = PROMOTIONS[choice] * f 
-                        selected_move = possible_moves[i]
-            if selected_move == None:
-                print("[ ]")
-                temp = [possible_moves[i].moveString for i in range(len(possible_moves))]                
-                break
-            else:
-                obj = one_hot_board(gs.board)
-                gs.makeMove(selected_move)
-        new_score = (found_moves / total_moves) * 100
-        scores.append(new_score)
-        print("(" + str(new_score) + "%)")
-    print("Hello World!")
-    return scores
+                else:
+                    total_fail += 1
+                    break
+        score = (score * 100) / (i+1)
+        print("Game: " + str(num+1) + " -- ( " + str(score) + "% )")
+    print("Number of Fails: " + str(total_fail) + " / " + str(num+1))
+    return total_fail
+
 
 def findGameStates():
     pgn_file = "./games/ficsgamesdb_202101_standard2000_nomovetimes_196479.pgn"
@@ -137,9 +90,9 @@ if __name__ == "__main__":
     plt.plot(x[1])
     plt.show()'''
     score = testEngine()
-    score.sort()
-    plt.plot(score)
-    plt.show()
+    #score.sort()
+    #plt.plot(score)
+    #plt.show()
 
-
+    
     print("Hello World")
